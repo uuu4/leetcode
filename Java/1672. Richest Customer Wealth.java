a4fe14f44add78d555d2ce04442e02edc66f4112
uuu4 <71949472+uuu4@users.noreply.github.com>
2024-04-19 12:43:24 +0300

    Create 1672. Richest Customer Wealth.java

---
+++ Java/1672. Richest Customer Wealth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        int sum=0;
+        for(int i=0;i<accounts.length;i++){
+            sum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum+= accounts[i][j];
+            }
+            if(max<sum){
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
